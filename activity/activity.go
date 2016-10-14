d1daac462c0927796dd242f8063f79a2f77c278f
Adro <shamaoxiaogui@gmail.com>
2016-10-14 19:04:53 +0800

    finish basic function and unit test passed

--- activity/activity.go
+++ activity/activity.go
@@ -5,6 +5,7 @@ package activity
 import (
 	"errors"
 	"fmt"
+	"time"
 )
 
 type Activity struct {
@@ -37,6 +38,11 @@ func (a *Activity) Parser(input string) error {
 	} else if a.begin < 0 || a.end < 0 || a.num < 0 {
 		return errors.New("invailed input: with minus???")
 	}
+
+	// re-initialize
+	a.income = 0
+	a.payment = 0
+	a.profit = 0
 	return nil
 }
 
@@ -51,7 +57,8 @@ func (a Activity) String() string {
 }
 
 // Calculate the number of yards need to rent
-func (a Activity) YardNumber() int {
+// When it return 0, the process(for one Activity) can be terminated
+func (a Activity) yardNumber() int {
 	t, x := a.num/6, a.num%6
 	var ret int
 	switch t {
@@ -75,4 +82,46 @@ func (a Activity) YardNumber() int {
 		ret = t
 	}
 	return ret
+}
+
+var timeLUT = [2][]int{
+	{9, 12, 18, 20, 22}, //Mon.-Fri.
+	{9, 12, 18, 22},     //Sat.-Sun.
+}
+var moneyLUT = [2][]int{
+	{30, 50, 80, 60}, //Mon.-Fri.
+	{40, 50, 60},     //Sat.-Sun.
+}
+
+func (a *Activity) CalcProfit() {
+	numYard := a.yardNumber()
+
+	if numYard != 0 {
+		// Calc Income
+		a.income = a.num * 30
+
+		//Calc Payment
+		const shortForm = "2016-10-14"
+		t, _ := time.Parse(shortForm, a.date)
+		index := 0 //default Mon.-Fri.
+		if wd := t.Weekday(); wd == time.Sunday || wd == time.Saturday {
+			index = 1
+		}
+		i := 0
+		for ; i < len(timeLUT[index])-1; i++ {
+			if a.begin >= timeLUT[index][i] && a.begin < timeLUT[index][i+1] {
+				break
+			}
+		}
+		i++
+		j := a.begin
+		for ; timeLUT[index][i] < a.end; i++ {
+			a.payment += numYard * moneyLUT[index][i-1] * (timeLUT[index][i] - j)
+			j = timeLUT[index][i]
+		}
+		a.payment += numYard * moneyLUT[index][i-1] * (a.end - j)
+
+		//Calc profit
+		a.profit = a.income - a.payment
+	} // else the activity is terminated, income=0, payment=0, profit=0
 }
--- activity/activity_test.go
+++ activity/activity_test.go
@@ -30,7 +30,7 @@ func TestActivityString1(t *testing.T) {
 	act.profit = 3
 	ret := "2016-06-03 09:00~12:00 +0 -0 +3"
 	if str := act.String(); str != ret {
-		t.Error("Activity String() failed. Got %s, should be %s", str, ret)
+		t.Error(fmt.Sprintf("Activity String() failed. Got %s, should be %s", str, ret))
 	}
 }
 func TestActivityString2(t *testing.T) {
@@ -39,7 +39,7 @@ func TestActivityString2(t *testing.T) {
 	act.profit = -3
 	ret := "2016-06-03 09:00~12:00 +0 -0 -3"
 	if str := act.String(); str != ret {
-		t.Error("Activity String() failed. Got %s, should be %s", str, ret)
+		t.Error(fmt.Sprintf("Activity String() failed. Got %s, should be %s", str, ret))
 	}
 }
 func TestActivityString3(t *testing.T) {
@@ -47,62 +47,98 @@ func TestActivityString3(t *testing.T) {
 	act.Parser("2016-06-03 09:00~12:00 14")
 	ret := "2016-06-03 09:00~12:00 +0 -0 0"
 	if str := act.String(); str != ret {
-		t.Error("Activity String() failed. Got %s, should be %s", str, ret)
+		t.Error(fmt.Sprintf("Activity String() failed. Got %s, should be %s", str, ret))
 	}
 }
-func TestActivityYardNumber1(t *testing.T) {
+func TestActivityyardNumber1(t *testing.T) {
 	var act Activity
 	act.Parser("2016-06-03 09:00~12:00 0")
-	if n := act.YardNumber(); n != 0 {
-		t.Error("Activity YardNumber() failed. Got %d, should be %d",
-			n, 0)
+	if n := act.yardNumber(); n != 0 {
+		t.Error(fmt.Sprintf("Activity yardNumber() failed. Got %d, should be %d",
+			n, 0))
 	}
 }
-func TestActivityYardNumber2(t *testing.T) {
+func TestActivityyardNumber2(t *testing.T) {
 	var act Activity
 	act.Parser("2016-06-03 09:00~12:00 3")
-	if n := act.YardNumber(); n != 0 {
-		t.Error("Activity YardNumber() failed. Got %d, should be %d",
-			n, 0)
+	if n := act.yardNumber(); n != 0 {
+		t.Error(fmt.Sprintf("Activity yardNumber() failed. Got %d, should be %d",
+			n, 0))
 	}
 }
-func TestActivityYardNumber3(t *testing.T) {
+func TestActivityyardNumber3(t *testing.T) {
 	var act Activity
 	act.Parser("2016-06-03 09:00~12:00 4")
-	if n := act.YardNumber(); n != 1 {
-		t.Error("Activity YardNumber() failed. Got %d, should be %d",
-			n, 1)
+	if n := act.yardNumber(); n != 1 {
+		t.Error(fmt.Sprintf("Activity yardNumber() failed. Got %d, should be %d",
+			n, 1))
 	}
 }
-func TestActivityYardNumber4(t *testing.T) {
+func TestActivityyardNumber4(t *testing.T) {
 	var act Activity
 	act.Parser("2016-06-03 09:00~12:00 7")
-	if n := act.YardNumber(); n != 2 {
-		t.Error("Activity YardNumber() failed. Got %d, should be %d",
-			n, 2)
+	if n := act.yardNumber(); n != 2 {
+		t.Error(fmt.Sprintf("Activity yardNumber() failed. Got %d, should be %d",
+			n, 2))
 	}
 }
-func TestActivityYardNumber5(t *testing.T) {
+func TestActivityyardNumber5(t *testing.T) {
 	var act Activity
 	act.Parser("2016-06-03 09:00~12:00 14")
-	if n := act.YardNumber(); n != 2 {
-		t.Error("Activity YardNumber() failed. Got %d, should be %d",
-			n, 2)
+	if n := act.yardNumber(); n != 2 {
+		t.Error(fmt.Sprintf("Activity yardNumber() failed. Got %d, should be %d",
+			n, 2))
 	}
 }
-func TestActivityYardNumber6(t *testing.T) {
+func TestActivityyardNumber6(t *testing.T) {
 	var act Activity
 	act.Parser("2016-06-03 09:00~12:00 16")
-	if n := act.YardNumber(); n != 3 {
-		t.Error("Activity YardNumber() failed. Got %d, should be %d",
-			n, 3)
+	if n := act.yardNumber(); n != 3 {
+		t.Error(fmt.Sprintf("Activity yardNumber() failed. Got %d, should be %d",
+			n, 3))
 	}
 }
-func TestActivityYardNumber7(t *testing.T) {
+func TestActivityyardNumber7(t *testing.T) {
 	var act Activity
 	act.Parser("2016-06-03 09:00~12:00 25")
-	if n := act.YardNumber(); n != 4 {
-		t.Error("Activity YardNumber() failed. Got %d, should be %d",
-			n, 4)
+	if n := act.yardNumber(); n != 4 {
+		t.Error(fmt.Sprintf("Activity yardNumber() failed. Got %d, should be %d",
+			n, 4))
+	}
+}
+
+var testInput = [...]string{
+	"2016-06-02 20:00~22:00 7",
+	"2016-06-03 09:00~12:00 14",
+	"2016-06-04 14:00~17:00 22",
+	"2016-06-05 19:00~22:00 3",
+	"2016-06-06 12:00~15:00 15",
+	"2016-06-07 15:00~17:00 12",
+	"2016-06-08 10:00~13:00 19",
+	"2016-06-09 16:00~18:00 16",
+	"2016-06-10 20:00~22:00 5",
+	"2016-06-11 13:00~15:00 11",
+}
+var testExpected = [...]string{
+	"2016-06-02 20:00~22:00 +210 -240 -30",
+	"2016-06-03 09:00~12:00 +420 -180 +240",
+	"2016-06-04 14:00~17:00 +660 -600 +60",
+	"2016-06-05 19:00~22:00 +0 -0 0",
+	"2016-06-06 12:00~15:00 +450 -300 +150",
+	"2016-06-07 15:00~17:00 +360 -200 +160",
+	"2016-06-08 10:00~13:00 +570 -330 +240",
+	"2016-06-09 16:00~18:00 +480 -300 +180",
+	"2016-06-10 20:00~22:00 +150 -120 +30",
+	"2016-06-11 13:00~15:00 +330 -200 +130",
+}
+
+func TestActivityCalcProfit1(t *testing.T) {
+	var act Activity
+	for i := 0; i < len(testInput); i++ {
+		act.Parser(testInput[i])
+		act.CalcProfit()
+		if str := act.String(); str != testExpected[i] {
+			t.Error(fmt.Sprintf("Activity String() failed. Got %s, should be %s", str, testExpected[i]))
+		}
 	}
 }
